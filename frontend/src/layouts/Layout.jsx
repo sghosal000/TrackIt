17fefff684fb8f01bea100051dce7456118639ac
sghosal <sghosal@test.com>
2024-04-26 15:00:57 +0530

    added basic folder structure for frontend

---
+++ frontend/src/layouts/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from './Navbar'
+import Navbar from './Footer'
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="container py-1">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
