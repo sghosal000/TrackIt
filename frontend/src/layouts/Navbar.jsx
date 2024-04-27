1ebd6363e67612ec395ce4cc60fa4163ac652114
Bhakti Prakash Das <prakashbhakti2@gmail.com>
2024-04-27 12:14:08 +0530

    ui changes

--- frontend/src/layouts/Navbar.jsx
+++ frontend/src/layouts/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full h-16 bg-white shadow-md flex items-center justify-between px-4">
       {/* Brand Name (Left) */}
-      <h1 className="text-xl font-bold text-gray-800">TrackIt</h1>
+      <h1 className="text-xl font-bold text-gray-800"><Link to="/">TrackIt</Link></h1>
 
       {/* Login/Signup Options (Right) */}
       <ul className="flex space-x-4">
--- frontend/src/pages/BudgetContent.jsx
+++ frontend/src/pages/BudgetContent.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import Progressbar from "./Progress_bar";
 
 function BudgetContent() {
   return (
-    <>
-      <div className='mt-32'>
+    <div class="row flex">
+      <div className='col mt-32 w-1/2'>
         budget
       </div>
-    </>
+      <div className='col mt-32 w-1/2'>
+      <div className="App">
+            <h3 className="heading">Progress Bar</h3>
+            <Progressbar
+                progress="30"
+                height={30}
+            />
+            <Progressbar
+                progress="81"
+                height={30}
+            />
+            
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
