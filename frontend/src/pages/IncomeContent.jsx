5505d093ed1b0962a374f190a1ff30465b923302
Bhakti Prakash Das <prakashbhakti2@gmail.com>
2024-04-27 11:19:55 +0530

    UI

--- frontend/src/pages/IncomeContent.jsx
+++ frontend/src/pages/IncomeContent.jsx
@@ -4,11 +4,11 @@ import TransactionForm from './TransactionForm'
 function IncomeContent({ incomes }) {
 	return (
 		<div className="flex justify-center mt-32">
-			<div className="w-1/2 pr-4">
+			<div className="w-1/3 pr-4">
 				<TransactionForm type={ "income" }/>
 			</div>
-			<div className="w-1/2 pl-4">
-				<div className="bg-gray-100 p-4 h-64 overflow-y-auto">
+			<div className="w-2/3 pl-4">
+				<div className="bg-gray-100 p-4 max-h-128 overflow-y-auto">
 					<table className="w-full">
 						<thead>
 							<tr>
@@ -21,10 +21,10 @@ function IncomeContent({ incomes }) {
 						<tbody className='bg-green-100'>
 							{incomes && incomes.map(income =>
 								<tr key={income._id}>
-									<td className="border-b py-2">{income.createdAt.split('T')[0]}</td>
-									<td className="border-b py-2">{income.amount}</td>
-									<td className="border-b py-2">{income.categoryid}</td>
-									<td className="border-b py-2">{income.note}</td>
+									<td className="border-b py-2 text-center">{income.createdAt.split('T')[0]}</td>
+									<td className="border-b py-2 text-center">{income.amount}</td>
+									<td className="border-b py-2 text-center">{income.categoryid}</td>
+									<td className="border-b py-2 text-center">{income.note}</td>
 								</tr>
 							)}
 						</tbody>
